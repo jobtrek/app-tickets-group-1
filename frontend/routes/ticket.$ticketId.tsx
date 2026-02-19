12ee44c391d875b5ef929dd7e976fb443ae12978
thomaslucking <telucking@gmail.com>
2026-02-19 16:05:39 +0100

    fix: fixed routing for it displays creation ticket form

--- frontend/routes/ticket.$ticketId.tsx
+++ frontend/routes/ticket.$ticketId.tsx
@@ -1,11 +1,9 @@
 import { rootRoute } from "./__root";
 import { createRoute } from "@tanstack/react-router";
+import TicketCreation from "../src/pages/TicketCreation.tsx"
 
 export const ticketInformationRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: "/ticket/$ticketId",
-  component: () => {
-    const { ticketId } = ticketInformationRoute.useParams();
-    return <h1>Ticket: {ticketId}</h1>;
-  },
+  path: "/ticket",
+  component: () => < TicketCreation />
 });
--- frontend/src/pages/TicketCreation.tsx
+++ frontend/src/pages/TicketCreation.tsx
@@ -1,5 +1,6 @@
 import Form from '../components/Form';
 
+
 export default function TicketCreation() {
   return (
     <div className='min-h-screen w-full flex flex-col items-center py-24 px-4'>
