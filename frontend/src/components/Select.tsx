1b87b3945f7fe99870f0f64540e6cf2d4eb40f9f
wasp32679 <ryan.kenmoe@outlook.fr>
2026-02-19 15:33:17 +0100

    feat: other half of changes

---
+++ frontend/src/components/Select.tsx
@@ -0,0 +1,21 @@
+interface SelectProps {
+  id: string;
+}
+
+export default function Select({ id }: SelectProps) {
+  return (
+    <>
+      <select
+        name={id}
+        id={id}
+        className='w-full border-gray-400 border border-solid rounded-xl p-4 placeholder:text-gray-400'
+      >
+        <option value=''>Indiquez le niveau d'urgence</option>
+        <option value='bas'>Bas</option>
+        <option value='moyen'>Moyen</option>
+        <option value='haut'>Haut</option>
+        <option value='urgent'>Urgent</option>
+      </select>
+    </>
+  );
+}
