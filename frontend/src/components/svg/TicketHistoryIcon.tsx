44323e5b910193300556f58253ec942360ccf92e
thomaslucking <telucking@gmail.com>
2026-02-24 09:40:17 +0100

    feat: improved navbar and fixed routing

---
+++ frontend/src/components/svg/TicketHistoryIcon.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+export const TicketHistoryIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={17}
+    height={17}
+    fill="none"
+
+  >
+    <path
+      stroke="#D1CBCB"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M1 15.4V13m7.2 2.4V7m7.2 8.4V1"
+    />
+  </svg>
+)
+export default TicketHistoryIcon
--- frontend/src/pages/Navbar.tsx
+++ frontend/src/pages/Navbar.tsx
@@ -1,11 +1,15 @@
 import { NavbarButton } from "../components/NavbarButton";
+import { useNavigate } from "@tanstack/react-router";
+import { DashboardIcon } from "../components/svg/DashboardIcon";
+import { TicketHistoryIcon } from "../components/svg/TicketHistoryIcon";
+import { StatisticsIcon } from "../components/svg/StatisticsIcon";
+
 export function Navbar() {
   return (
-    <div className="flex flex-col gap-3 m-3">
-        <NavbarButton text="Dashboard" />
-        <NavbarButton text="Tickets History" /> 
-        <NavbarButton text="Statistics" /> 
+    <div className="flex flex-col gap-3 m-3 h-full w-48 items-center px-8 py-4 ">
+      <NavbarButton Icon={<DashboardIcon />} text="Dashboard" />
+      <NavbarButton Icon={<TicketHistoryIcon />} text="Tickets History" />
+      <NavbarButton Icon={<StatisticsIcon />} text="Statistics" />
     </div>
-    
   );
 }
