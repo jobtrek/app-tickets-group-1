5dbb39f2a1e43eb45a55b3b8d0c6a9d7fd79642f
wasp32679 <ryan.kenmoe@outlook.fr>
2026-03-02 11:40:30 +0100

    refactor: handling post request on form + switched from hardcoded data to props in ticketview

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,11 +1,17 @@
+import type { ButtonHTMLAttributes } from 'react';
+
 interface ButtonProps {
   title: string;
+  type: ButtonHTMLAttributes<HTMLButtonElement>['type'];
 }
 
-export default function Button({ title }: ButtonProps) {
+export default function Button({ title, type }: ButtonProps) {
   return (
     <>
-      <button className='w-full text-2xl p-4 text-white bg-blue-400 rounded-xl'>
+      <button
+        type={type}
+        className='w-full text-2xl p-4 text-white bg-blue-400 rounded-xl'
+      >
         {title}
       </button>
     </>
--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,3 +1,4 @@
+import { useActionState } from 'react';
 import Button from './Button';
 import FormField from './FormField';
 import InputFile from './InputFile';
@@ -6,9 +7,24 @@ import Select from './Select';
 import TextArea from './TextArea';
 
 export default function Form() {
+  const [state, action, pending] = useActionState(
+    async (_: unknown, formData: FormData) => {
+      try {
+        // await addTicket(formData);
+        return 'Ticket added !';
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    null,
+  );
+
   return (
     <>
-      <form className='flex flex-col gap-12 bg-white p-8 border-2 border-solid rounded-xl border-gray-100'>
+      <form
+        action={action}
+        className='flex flex-col gap-12 bg-white p-8 border-2 border-solid rounded-xl border-gray-100'
+      >
         <h1 className='self-center text-4xl pt-4'>Nouveau Ticket</h1>
 
         <FormField label='Titre' id='title'>
@@ -27,8 +43,17 @@ export default function Form() {
           <InputFile id='img' />
         </FormField>
 
-        <Button title='Créer un ticket' />
+        <Button type='submit' title='Créer un ticket' />
       </form>
+      {(pending || state) && (
+        <div className='state-text'>
+          {pending ? (
+            <span className='loading loading-spinner loading-xl'></span>
+          ) : (
+            state
+          )}
+        </div>
+      )}
     </>
   );
 }
