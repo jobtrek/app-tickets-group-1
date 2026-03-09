18c02391f0e5e4d76af7284f99917be0eb8bfa32
thomaslucking <telucking@gmail.com>
2026-03-09 15:17:14 +0100

    fix: added validation schema for users and post ticket endpoint

--- backend/src/controllers/ticket.controller.ts
+++ backend/src/controllers/ticket.controller.ts
@@ -1,7 +1,10 @@
 import { TicketStatus } from "backend/utils/constants";
+import * as v from "valibot";
 import { headers } from "../../utils/headers";
-import { db } from "../db/database";
+import { db } from "../db/database.ts";
 import { queries } from "../repositories/ticketQuery";
+import { TicketPostSchema } from "../validators/ticket.validator.ts";
+
 export const getResponse = () => {
 	try {
 		const tickets = db.query(queries.tickets.getAll).all();
@@ -15,19 +18,22 @@ export const insertResponse = async (req: Request): Promise<Response> => {
 	try {
 		const body = await req.json();
 
-		const { title, description, level, id_user } = body;
+		const validBody = v.safeParse(TicketPostSchema, body);
+
+		if (!validBody.success) {
+			return Response.json(
+				{ errors: validBody.issues.map((i) => i.message) },
+				{ status: 400, headers },
+			);
+		}
+
+		const { title, description, level, id_user } = validBody.output;
+
 		const defaultStatus = TicketStatus.Opened;
 		const insert = db.prepare(queries.tickets.insert);
 
-		const result = insert.get(
-			title,
-			description,
-			level,
-			defaultStatus,
-			id_user,
-		);
-
-		return Response.json(result, {
+		insert.get(title, description, level ?? null, defaultStatus, id_user);
+		return new Response("Ticket created successfully", {
 			status: 201,
 			headers,
 		});
