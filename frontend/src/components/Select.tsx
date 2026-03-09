81a0f2802831780831de1be8c0b21eb28fd4aec4
thomaslucking <telucking@gmail.com>
2026-03-09 11:49:30 +0100

    feat: added general .vscode settings for automatique formating

--- frontend/src/components/Select.tsx
+++ frontend/src/components/Select.tsx
@@ -10,18 +10,16 @@ interface SelectProps {
 
 export default function Select({ id, options }: SelectProps) {
 	return (
-		<>
-			<select
-				name={id}
-				id={id}
-				className="w-full border-gray-400 border border-solid rounded-xl p-4 placeholder:text-gray-400"
-			>
-				{options.map((o) => (
-					<option key={o.value} value={o.value}>
-						{o.label}
-					</option>
-				))}
-			</select>
-		</>
+		<select
+			name={id}
+			id={id}
+			className="w-full border-gray-400 border border-solid rounded-xl p-4 placeholder:text-gray-400"
+		>
+			{options.map((o) => (
+				<option key={o.value} value={o.value}>
+					{o.label}
+				</option>
+			))}
+		</select>
 	);
 }
