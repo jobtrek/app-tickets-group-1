052411dcb4497dd50f694072124e880e709bd225
wasp32679 <ryan.kenmoe@outlook.fr>
2026-03-10 13:27:00 +0100

    fix: forms style inconsistencies and improved components reusability

--- frontend/src/components/FormField.tsx
+++ frontend/src/components/FormField.tsx
@@ -9,7 +9,10 @@ interface FormFieldProps {
 export default function FormField({ id, label, children }: FormFieldProps) {
 	return (
 		<div className="flex flex-col gap-2 w-full">
-			<label htmlFor={id} className="text-2xl">
+			<label
+				htmlFor={id}
+				className="block text-sm font-medium text-gray-700 mb-1.5"
+			>
 				{label}
 			</label>
 			{children}
