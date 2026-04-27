22622599463e359606153c244de5891b5e578197
thomaslucking <telucking@gmail.com>
2026-04-27 16:06:07 +0200

    fix: fixed duplication ticket creation

--- frontend/routes/_adminOnly/dashboard.tsx
+++ frontend/routes/_adminOnly/dashboard.tsx
@@ -4,7 +4,7 @@ import Dashboard from "../../src/pages/Dashboard";
 import { useTicketStore } from "../../src/store/ticketStore";
 import { apiClient } from "../../src/utils/clientApi";
 import type { Ticket } from "../../src/utils/types";
-import { useTicketListUpdates } from "../../src/utils/useTicketListUpdates";
+// import { useTicketListUpdates } from "../../src/utils/useTicketListUpdates";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -19,7 +19,6 @@ export const Route = createFileRoute("/_adminOnly/dashboard")({
 function DashboardPage() {
 	const loaderTickets = Route.useLoaderData();
 	const setTickets = useTicketStore((state) => state.setTickets);
-	useTicketListUpdates();
 
 	useEffect(() => {
 		setTickets(loaderTickets);
